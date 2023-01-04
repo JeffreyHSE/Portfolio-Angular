855eaf4f4c6374ad638134ff4a13225070ebae9b
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2023-01-04 14:40:39 +0100

    added mobileNavbar() methode in app.component.ts that controls the navigation bar

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -42,4 +42,31 @@ export class AppComponent {
     // @ts-ignore
     window.open("https://github.com/JeezyDev/Bookmark-Landing-Page", '_blank').focus();
   }
+
+  mobileNavbar(): void {
+     let get_icon = document.getElementById("mobile-dropdown-menu");
+     let mobile_nav = document.createElement("div");
+     mobile_nav.innerHTML = `
+    <ul style="margin: 0; padding: 0;  display: flex; flex-direction: column; list-style-type: none; gap: .4rem; background-color: #fff; align-items: center;">
+    <li style="font-weight: bold; width: 100%; display: flex; justify-content: center; border-bottom: 1px solid #444;">
+    <a style="text-decoration: none; color: black;" href="#projects-container">Projects</a>
+      </li>
+      <li style="font-weight: bold; width: 100%; display: flex; justify-content: center; border-bottom: 1px solid #444;">
+    <a style="text-decoration: none; color: black;" href="#header-section-desktop">About</a>
+      </li>
+      <li style="font-weight: bold">
+    <a style="text-decoration: none; color: black;" href="#contact">Contact</a>
+      </li>
+      </ul>`;
+      // @ts-ignore
+      if (get_icon.innerText === "☰") {
+        // @ts-ignore
+        get_icon.innerText = "x";
+        // @ts-ignore
+        get_icon.insertAdjacentElement("afterbegin", mobile_nav);
+      } else {
+        // @ts-ignore
+        get_icon.innerText = "☰";
+      }
+    }
  }
